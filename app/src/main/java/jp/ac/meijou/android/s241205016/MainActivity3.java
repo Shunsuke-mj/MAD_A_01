aced95e2ea8dbfcab880d3fdcde34de4c3b22efa
Shunsuke-mj <183798932+Shunsuke-mj@users.noreply.github.com>
2025-09-03 14:38:19 +0900

    actibity_main2.xmlにテキストボックスと送信ボタンを追加

--- app/src/main/java/jp/ac/meijou/android/s241205016/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s241205016/MainActivity3.java
@@ -14,8 +14,10 @@ import jp.ac.meijou.android.s241205016.databinding.ActivityMainBinding;
 public class MainActivity3 extends AppCompatActivity {
 
     private ActivityMain3Binding binding;
-    private int cal_ans = 0;
+    private double cal_ans = 0;
+    private double cal_in = 0;
     private int cal_o = 0;
+    private int cal_j = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,29 +34,70 @@ public class MainActivity3 extends AppCompatActivity {
         //演算子
         binding.buttonCalp.setOnClickListener(view -> {
             cal_o = 1;
+            cal_j = 1;
             binding.textViewCalo.setText("+");
         });
         binding.buttonCalm.setOnClickListener(view -> {
             cal_o = 2;
+            cal_j = 1;
             binding.textViewCalo.setText("-");
         });
         binding.buttonCalx.setOnClickListener(view -> {
             cal_o = 3;
+            cal_j = 1;
             binding.textViewCalo.setText("×");
         });
         binding.buttonCald.setOnClickListener(view -> {
             cal_o = 4;
+            cal_j = 1;
             binding.textViewCalo.setText("÷");
+        });
+        binding.buttonCale.setOnClickListener(view -> {
+            if (cal_o == 1) {
+                cal_ans = cal_ans + cal_in;
+                binding.textViewCal.setText(String.valueOf(cal_ans));
+                cal_in = 0;
+                cal_o = 0;
+                cal_j = 0;
+            } else if (cal_o == 2) {
+                cal_ans = cal_ans - cal_in;
+                binding.textViewCal.setText(String.valueOf(cal_ans));
+                cal_in = 0;
+                cal_o = 0;
+                cal_j = 0;
+            } else if (cal_o == 3) {
+                cal_ans = cal_ans * cal_in;
+                binding.textViewCal.setText(String.valueOf(cal_ans));
+                cal_in = 0;
+                cal_o = 0;
+                cal_j = 0;
+            } else if (cal_o == 4) {
+                if (cal_in == 0) {
+                    binding.textViewCal.setText("ERROR");
+                } else {
+                    cal_ans = cal_ans / cal_in;
+                    binding.textViewCal.setText(String.valueOf(cal_ans));
+                }
+                cal_in = 0;
+                cal_o = 0;
+                cal_j = 0;
+            }
         });
 
         //AC
         binding.buttonCalAC.setOnClickListener(view -> {
             cal_o = 0;
             cal_ans = 0;
+            cal_in = 0;
+            cal_j = 0;
             binding.textViewCalo.setText("");
             binding.textViewCal.setText("0");
+            binding.textViewCalin.setText("0");
         });
 
-        //数字(未実装)
+        //数字
+        binding.buttonCal0.setOnClickListener(view -> {
+
+        });
     }
 }
